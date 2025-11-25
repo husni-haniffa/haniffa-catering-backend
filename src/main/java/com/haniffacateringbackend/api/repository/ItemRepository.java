4b8b3382d85edf1a41eead09f088018891e404ce
Husni <husniwfayo@gmail.com>
2025-11-25 23:11:57 +0530

    added repository annotation

--- src/main/java/com/haniffacateringbackend/api/repository/ItemRepository.java
+++ src/main/java/com/haniffacateringbackend/api/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.haniffacateringbackend.api.repository;
 
 import com.haniffacateringbackend.api.model.Item;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends MongoRepository<Item, String> {
 }
