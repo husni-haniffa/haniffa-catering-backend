9b3627f2fc283e618a029cf3c482dd3f2b4b8eff
Husni <husniwfayo@gmail.com>
2025-11-25 21:43:00 +0530

    intial setup and connected mongodb

---
+++ src/main/java/com/haniffacateringbackend/api/HaniffaCateringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.haniffacateringbackend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaniffaCateringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaniffaCateringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haniffacateringbackend/api/HaniffaCateringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haniffacateringbackend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaniffaCateringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
