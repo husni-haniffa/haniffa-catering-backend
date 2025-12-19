727bee0593f055df0ae4ef847956c3766dd39203
Husni <husniwfayo@gmail.com>
2025-12-19 15:33:15 +0530

    added global exceptions

---
+++ src/main/java/com/haniffacateringbackend/api/middlewares/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.haniffacateringbackend.api.middlewares;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                404,
+                ex.getMessage(),
+                "Requested resource not found",
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                500,
+                ex.getMessage(),
+                "Unexpected error occurred",
+                LocalDateTime.now()
+        );
+         return  new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
